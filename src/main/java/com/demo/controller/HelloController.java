92492c68e5518547abdc4f62a67b45cf4b1e6914
Ashwini Kumar <ashwinikumar20035@gmail.com>
2025-11-26 14:23:30 +0530

    first commit

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    
+    @GetMapping("")
+    public String getMethodName() {
+        return new String("Hello World, this is my first Jenkin job!!");
+    }
+    
+}
